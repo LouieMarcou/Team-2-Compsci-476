7806729bacd81bddaa2cabdda362ddc05b4443db
David <Dayved28@gmail.com>
2022-11-15 18:01:08 -0600

    practice sdk request working

---
+++ Website/create_price.php
@@ -0,0 +1,20 @@
+<?php
+require_once('vendor/autoload.php');
+
+$stripe = new \Stripe\StripeClient("sk_test_51M32h7CW4FUqp8zTkwPns6CMxp1eVC5m4kbCIPmevxQ8llDbBc8muDCRiizvn9168dXIBSvVhfnU1daWP5ZF8lsx00DQvuq3CW");
+
+$product = $stripe->products->create([
+  'name' => 'Starter Subscription',
+  'description' => '$12/Month subscription',
+]);
+echo "Success! Here is your starter subscription product id: " . $product->id . "\n";
+
+$price = $stripe->prices->create([
+  'unit_amount' => 1200,
+  'currency' => 'usd',
+  'recurring' => ['interval' => 'month'],
+  'product' => $product['id'],
+]);
+echo "Success! Here is your premium subscription price id: " . $price->id . "\n";
+
+?>
