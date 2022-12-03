b336c4fb0a22fc963beb5622dce34517883e7060
AbbyB44 <butterhoops44minecraft@gmail.com>
2022-12-02 21:19:56 -0600

    Testing switch

--- Website/database.php
+++ Website/database.php
@@ -26,101 +26,130 @@
 		</div>
 <?php
 
-    $user = 'belschneal15';
-    $pass = 'ab3993';
-    $db_info = 'mysql:dbname=cornersidehelp;host=cs.uww.edu';
+    // $user = 'belschneal15';
+    // $pass = 'ab3993';
+    // $db_info = 'mysql:dbname=cornersidehelp;host=cs.uww.edu';
     //$db_info = 'mysql:host=cs.uww.edu;dbname=cornersidehelp';
 
-    try {
-        $db = mysqli_connect('localhost', 'root', '', 'cornersidehelp');
+    $db = mysqli_connect('localhost', 'root', '', 'cornersidehelp');
         echo '<p> here </p>';
         if(!$db) {
             echo '<p> failed </p>';
         }
-        //$db = new PDO($db_info, $user, $pass);
-    }
-    catch (PDOException $e) {
-        print "Error!: " . $e->getmessage() . "<br>";
-        die();
-    }
+
+        if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['city'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $city = $_POST['city'];
+        }
+
+        $sql = 'INSERT INTO users(firstName, lastName, city, pHash, username) 
+        VALUES (:firstName,:lastName,:city,:pHash,:username)';
+
+        $pHash = password_hash($password, PASSWORD_DEFAULT);
+
+        $parameters = [
+            ":firstName" => $firstName,
+            ":lastName" => $lastName,
+            ":city" => $city,
+            ":pHash" => $pHash,
+            "username" => $username
+        ];
+        mysqli_query($db, $sql);
+        echo('here');
+
+    // try {
+    //     $db = mysqli_connect('localhost', 'root', '', 'cornersidehelp');
+    //     echo '<p> here </p>';
+    //     if(!$db) {
+    //         echo '<p> failed </p>';
+    //     }
+    //     //$db = new PDO($db_info, $user, $pass);
+    // }
+    // catch (PDOException $e) {
+    //     print "Error!: " . $e->getmessage() . "<br>";
+    //     die();
+    // }
 
     //CONNECTION TO DATABASE 
 
-    $mode = "";
+    // $mode = "";
 
-    try {
-        if(isset($_GET['mode'])) {
-            $mode = $_GET['mode'];
-        }
+    // try {
+    //     if(isset($_GET['mode'])) {
+    //         $mode = $_GET['mode'];
+    //     }
     
 
-        switch ($mode) {
-            case 'displayProfile':
-                include('Profile.html');
+    //     switch ($mode) {
+    //         case 'displayProfile':
+    //             include('Profile.html');
 
-                if ((isset($_POST['username']) && (isset($_POST['password'])))) {
-                    $username = $_POST['username'];
-                    $password = $_POST['password'];
-                    $pHash = password_hash($password, PASSWORD_DEFAULT);
-                }
+    //             if ((isset($_POST['username']) && (isset($_POST['password'])))) {
+    //                 $username = $_POST['username'];
+    //                 $password = $_POST['password'];
+    //                 $pHash = password_hash($password, PASSWORD_DEFAULT);
+    //             }
 
-                $parameters = [
-                    ":username" => $username,
-                    "pHash" => $pHash
-                ];
+    //             $parameters = [
+    //                 ":username" => $username,
+    //                 "pHash" => $pHash
+    //             ];
 
-                $sql = 'SELECT `firstName`, `lastName`, `accountBalance`, `city`, `shelterID` FROM `users` WHERE `username` = :username AND `pHash` = :pHash';
+    //             $sql = 'SELECT `firstName`, `lastName`, `accountBalance`, `city`, `shelterID` FROM `users` WHERE `username` = :username AND `pHash` = :pHash';
 
-                $resultSet = prep($sql, $db, $parameterValues);
+    //             $resultSet = prep($sql, $db, $parameterValues);
 
                 
 
-                // $pageTitle = 'Profile';
-                // $columns = array("");
-                // displayResultSet($pageTitle, $reultSet, $columns);
+    //             // $pageTitle = 'Profile';
+    //             // $columns = array("");
+    //             // displayResultSet($pageTitle, $reultSet, $columns);
 
-                break;
+    //             break;
 
-            case 'createUser':
+    //         case 'createUser':
 
-                $sql = 'INSERT INTO `users`(`firstName`, `lastName`, `city`, `pHash`, `username`) 
-                VALUES (:firstName,:lastName,:city,:pHash,:username)';
+    //             $sql = 'INSERT INTO `users`(`firstName`, `lastName`, `city`, `pHash`, `username`) 
+    //             VALUES (:firstName,:lastName,:city,:pHash,:username)';
 
-                if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['city'])) {
-                    $username = $_POST['username'];
-                    $password = $_POST['password'];
-                    $firstName = $_POST['firstName'];
-                    $lastName = $_POST['lastName'];
-                    $city = $_POST['city'];
-                }
+    //             if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['city'])) {
+    //                 $username = $_POST['username'];
+    //                 $password = $_POST['password'];
+    //                 $firstName = $_POST['firstName'];
+    //                 $lastName = $_POST['lastName'];
+    //                 $city = $_POST['city'];
+    //             }
 
-                $pHash = password_hash($password, PASSWORD_DEFAULT);
+    //             $pHash = password_hash($password, PASSWORD_DEFAULT);
 
-                $parameters = [
-                    ":firstName" => $firstName,
-                    ":lastName" => $lastName,
-                    ":city" => $city,
-                    ":pHash" => $pHash,
-                    "username" => $username
-                ];
-                mysqli_query($db, $sql);
-                echo('here');
-                //$stmt = $db->prepare($sql);
+    //             $parameters = [
+    //                 ":firstName" => $firstName,
+    //                 ":lastName" => $lastName,
+    //                 ":city" => $city,
+    //                 ":pHash" => $pHash,
+    //                 "username" => $username
+    //             ];
+    //             mysqli_query($db, $sql);
+    //             echo('here');
+    //             //$stmt = $db->prepare($sql);
 
-                // if($stmt->execute($parameters)) {
-                //     echo "Created New User";
-                // } else {
-                //     $stmt->error;
-                // }
-                break;
-            default:
+    //             // if($stmt->execute($parameters)) {
+    //             //     echo "Created New User";
+    //             // } else {
+    //             //     $stmt->error;
+    //             // }
+    //             break;
+    //         default:
                 
-        }
-    }
-    catch (PDOException $e) {
-        echo "Error!: " . $e->getmessage() . "<br>";
-        die();
-    } 
+    //     }
+    // }
+    // catch (PDOException $e) {
+    //     echo "Error!: " . $e->getmessage() . "<br>";
+    //     die();
+    // } 
 ?>
 <form>
 	</span><!--span for text and button to be aligned-->
